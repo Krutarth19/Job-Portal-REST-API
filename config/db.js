d065648a481e781d8a25c89a2a096f16300e7818
Krutarth Patel <92313715+Krutarth19@users.noreply.github.com>
2023-04-19 23:41:22 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected! ${mongoose.connection.host}`)
     } catch (error) {
         console.log(`MongoDB Error ${error}`);
     }
